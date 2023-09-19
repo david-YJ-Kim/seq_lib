ddb627aca26930f3a93f58e07b5a098d71fa73eb
david <david@Absolicsinc.com>
2023-09-19 14:51:43 +0900

    SEQ Library 초기화 설정

--- src/main/java/com/abs/mes/SequenceManager.java
+++ src/main/java/com/abs/mes/SequenceManager.java
@@ -1,19 +1,54 @@
 package com.abs.mes;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public final class SequenceManager {
 
 
     public static void main(String[] args) {
+
+        String sourceSystem = "RTD"; // Property
+        String site = "SMV"; // Property
+        String env = "DEV"; // Property
+        List<Map> queryResult = null; // Select Query Result.
+
+        SequenceManager seqLib = new SequenceManager(sourceSystem, site, env, queryResult);
         System.out.println(
-                SequenceManager.getTargetName("WFS")
+                seqLib.getTargetName("WFS", "eventName", "payload")
         );
     }
 
-    public static String getTargetName(String targetSystem, String eqpId, String carrId, String lotId, String cid, String payload){
-        return getTargetName(targetSystem);
+    private String sourceSystem;
+    private String site;
+    private String env;
+    private List<Map> queryResult;
+    private String filPath;
+
+
+    public SequenceManager(String sourceSystem, String site, String env, List<Map> queryResult){
+
+        this.sourceSystem = sourceSystem;
+        this.site = site;
+        this.env = env;
+        this.queryResult = queryResult;
     }
 
-    public static String getTargetName(String targetSystem){
-        return "SVM/DEV/" + targetSystem + "/CMN/00";
+
+    public SequenceManager(String sourceSystem, String site, String env, String filePath){
+
+        this.sourceSystem = sourceSystem;
+        this.site = site;
+        this.env = env;
+        this.filPath = filePath;
     }
+
+
+
+    public String getTargetName(String targetSystem, String eventName, String payload){
+        return site + "/" + env + "/" + targetSystem + "/CMN/00";
+    }
+
+
 }
